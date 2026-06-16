e33324644171bbdf7101f568ee1234d4a50c1d35
vincennlin <vincentagwa@gmail.com>
2026-06-16 23:16:58 +0800

    Arrays & Hashing - Group Anagrams - ans

--- arrays-&-hashing/medium/group-anagrams/Solution.cpp
+++ arrays-&-hashing/medium/group-anagrams/Solution.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
@@ -10,7 +11,27 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        return {};
+        unordered_map<string, vector<string>> map;
+
+        for (string s : strs) {
+            vector<int> count(26, 0);
+            for (char c : s) {
+                count[c - 'a']++;
+            }
+            string key = to_string(count[0]);
+            for (int i = 1; i < 26; i++) {
+                key += ',' + to_string(count[i]);
+            }
+            map[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+
+        for (auto& [s, slist] : map) {
+            result.push_back(slist);
+        }
+
+        return result;
     }
 };
 
