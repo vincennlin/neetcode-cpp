70f25751354eeed0bd7d0e88f5bef24091e0d935
vincennlin <vincentagwa@gmail.com>
2026-06-16 23:07:54 +0800

    Arrays & Hashing - Two Sum - ans

--- arrays-&-hashing/easy/two-sum/Solution.cpp
+++ arrays-&-hashing/easy/two-sum/Solution.cpp
@@ -2,6 +2,7 @@
 // Created by vincenn lin on 2026/6/16.
 //
 
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
@@ -9,6 +10,20 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> indices;
+
+        for (int i = 0; i < n; i++) {
+            indices[nums[i]] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int diff = target - nums[i];
+            if (indices.count(diff) && indices[diff] != i) {
+                return {i, indices[diff]};
+            }
+        }
+
         return {};
     }
 };
