f06327204a731a0095c842160539b85197cf583a
vincennlin <vincentagwa@gmail.com>
2026-06-16 22:35:04 +0800

    Arrays & Hashing - Contains Duplicate - ans

--- arrays-&-hashing/easy/contains-duplicate/Solution.cpp
+++ arrays-&-hashing/easy/contains-duplicate/Solution.cpp
@@ -2,6 +2,7 @@
 // Created by vincenn lin on 2026/6/16.
 //
 
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
@@ -9,6 +10,14 @@ using namespace std;
 class Solution {
 public:
     bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (int num : nums) {
+            if (seen.count(num)) {
+                return true;
+            }
+            seen.insert(num);
+        }
+
         return false;
     }
 };
