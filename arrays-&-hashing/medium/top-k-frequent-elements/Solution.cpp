93fb49229ca77d58230fc1b187c61eda06e4a112
vincennlin <vincentagwa@gmail.com>
2026-06-16 23:36:43 +0800

    Arrays & Hashing - Top K Frequent Elements - ans

--- arrays-&-hashing/medium/top-k-frequent-elements/Solution.cpp
+++ arrays-&-hashing/medium/top-k-frequent-elements/Solution.cpp
@@ -3,12 +3,35 @@
 //
 
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        return {};
+        unordered_map<int, int> count;
+        vector<vector<int>> freq(nums.size() + 1);
+
+        for (int num : nums) {
+            count[num] = count[num] + 1;
+        }
+
+        for (const auto& entry : count) {
+            freq[entry.second].push_back(entry.first);
+        }
+
+        vector<int> result;
+
+        for (int i = freq.size() - 1; i > 0; i--) {
+            for (int n : freq[i]) {
+                result.push_back(n);
+                if (result.size() == k) {
+                    return result;
+                }
+            }
+        }
+
+        return result;
     }
 };
