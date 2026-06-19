51d9b99f1c85c8a23f137d76a5e461d1a444a42f
vincennlin <vincentagwa@gmail.com>
2026-06-19 23:50:01 +0800

    Arrays & Hashing - Products of Array Except Self - my revised

--- arrays-&-hashing/medium/products-of-array-except-self/Solution.cpp
+++ arrays-&-hashing/medium/products-of-array-except-self/Solution.cpp
@@ -20,27 +20,18 @@ public:
             }
         }
 
-        vector<int> result;
-
         if (zeroCount > 1) {
-            for (int i = 0; i < nums.size(); i++) {
-                result.push_back(0);
-            }
-            return result;
-        } else if (zeroCount == 1) {
-            for (int num : nums) {
-                if (num != 0) {
-                    result.push_back(0);
-                } else {
-                    result.push_back(products);
-                }
-            }
-        } else {
-            for (int num : nums) {
-                result.push_back(products / num);
-            }
+            return vector<int>(nums.size(), 0);
         }
 
+        vector<int> result(nums.size());
+        for (int i = 0; i < nums.size(); i++) {
+            if (zeroCount > 0) {
+                result[i] = (nums[i] == 0) ? products : 0;
+            } else {
+                result[i] = products / nums[i];
+            }
+        }
 
         return result;
     }
