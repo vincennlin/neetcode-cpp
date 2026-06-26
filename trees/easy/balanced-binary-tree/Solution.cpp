fd14c93b2f83b146554688fa19eabc8a2aedee57
vincennlin <vincentagwa@gmail.com>
2026-06-26 17:56:33 +0800

    Trees - Balanced Binary Tree - my

--- trees/easy/balanced-binary-tree/Solution.cpp
+++ trees/easy/balanced-binary-tree/Solution.cpp
@@ -3,10 +3,35 @@
 //
 
 #include "TreeNode.cpp"
+#include <algorithm>
+
+using namespace std;
 
 class Solution {
 public:
+    bool isBalancedTree;
+
     bool isBalanced(TreeNode* root) {
-        return false;
+        isBalancedTree = true;
+        maxDepth(root);
+        return isBalancedTree;
+    }
+
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        if (!isBalancedTree) {
+            return 0;
+        }
+
+        if (abs(leftDepth - rightDepth) > 1) {
+            isBalancedTree = false;
+            return 0;
+        }
+
+        return max(leftDepth, rightDepth) + 1;
     }
 };
