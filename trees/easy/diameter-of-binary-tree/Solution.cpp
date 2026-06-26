0e18b3e47d59698ca03c5ee49c4ec15dd6ce1f0c
vincennlin <vincentagwa@gmail.com>
2026-06-26 17:36:40 +0800

    Trees - Diameter of Binary Tree - ans

--- trees/easy/diameter-of-binary-tree/Solution.cpp
+++ trees/easy/diameter-of-binary-tree/Solution.cpp
@@ -3,10 +3,28 @@
 //
 
 #include "TreeNode.cpp"
+#include <algorithm>
+
+using namespace std;
 
 class Solution {
 public:
+    int maxDiameter;
+
     int diameterOfBinaryTree(TreeNode* root) {
-        return 0;
+        maxDiameter = 0;
+        maxDepth(root);
+        return maxDiameter;
+    }
+
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        maxDiameter = max(maxDiameter, leftDepth + rightDepth);
+
+        return max(leftDepth, rightDepth) + 1;
     }
 };
