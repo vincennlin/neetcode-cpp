4b72d5cb7077ab94629ae51c14661c304bbf0102
vincennlin <vincentagwa@gmail.com>
2026-06-26 16:52:50 +0800

    Linked List - Merge Two Sorted Linked Lists - my & ans

--- linked-list/easy/merge-two-sorted-linked-list/Solution.cpp
+++ linked-list/easy/merge-two-sorted-linked-list/Solution.cpp
@@ -7,6 +7,32 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        return nullptr;
+        ListNode *dummy = new ListNode(0);
+        ListNode *curr = dummy;
+
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                curr -> next = list1;
+                list1 = list1->next;
+            } else {
+                curr -> next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;
+        }
+
+        while (list1) {
+            curr -> next = list1;
+            list1 = list1->next;
+            curr = curr->next;
+        }
+
+        while (list2) {
+            curr -> next = list2;
+            list2 = list2->next;
+            curr = curr->next;
+        }
+
+        return dummy->next;
     }
 };
