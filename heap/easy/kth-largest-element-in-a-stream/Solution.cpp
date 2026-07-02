22d7d12621aa36d6448e1ac0c88f78e34fb01d40
vincennlin <vincentagwa@gmail.com>
2026-07-02 18:59:06 +0800

    Heap - Kth Largest Element in a Stream - ans

--- heap/easy/kth-largest-element-in-a-stream/Solution.cpp
+++ heap/easy/kth-largest-element-in-a-stream/Solution.cpp
@@ -2,16 +2,31 @@
 // Created by vincenn lin on 2026/7/2.
 //
 
+#include <queue>
 #include <vector>
 using namespace std;
 
 class KthLargest {
+private:
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    int k;
+
 public:
     KthLargest(int k, vector<int>& nums) {
-
+        this->k = k;
+        for (int num : nums) {
+            minHeap.push(num);
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
     }
 
     int add(int val) {
-        return 0;
+        minHeap.push(val);
+        if (minHeap.size() > k) {
+            minHeap.pop();
+        }
+        return minHeap.top();
     }
 };
