85dcc658eb27b51302ac2b7269ad5ff32aebecef
vincennlin <vincentagwa@gmail.com>
2026-07-02 17:08:05 +0800

    Trees - Subtree of Another Tree - ans

--- trees/easy/subtree-of-another-tree/Solution.cpp
+++ trees/easy/subtree-of-another-tree/Solution.cpp
@@ -6,6 +6,25 @@
 class Solution {
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (root == nullptr && subRoot == nullptr) return true;
+        if (root == nullptr) return false;
+
+        if (isSameTree(root, subRoot)) {
+            return true;
+        }
+
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true;
+
+        if (p != nullptr && q != nullptr) {
+            return p->val == q->val
+            && isSameTree(p->left, q->left)
+            && isSameTree(p->right, q->right);
+        }
+
         return false;
     }
 };
