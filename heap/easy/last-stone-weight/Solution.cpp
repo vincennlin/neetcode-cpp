24e62afd2579a24cb511d278f0a6a68983faf397
vincennlin <vincentagwa@gmail.com>
2026-07-02 19:10:51 +0800

    Heap - Last Stone Weight - ans

--- heap/easy/last-stone-weight/Solution.cpp
+++ heap/easy/last-stone-weight/Solution.cpp
@@ -2,12 +2,28 @@
 // Created by vincenn lin on 2026/7/2.
 //
 
+#include <queue>
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        return 0;
+        priority_queue<int> maxHeap;
+        for (int num : stones) {
+            maxHeap.push(num);
+        }
+
+        while (maxHeap.size() > 1) {
+            int x = maxHeap.top();
+            maxHeap.pop();
+            int y = maxHeap.top();
+            maxHeap.pop();
+            if (x > y) {
+                maxHeap.push(x - y);
+            }
+        }
+
+        return maxHeap.size() == 1 ? maxHeap.top() : 0;
     }
 };
