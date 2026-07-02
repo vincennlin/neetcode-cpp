a586a8d6e8c37fa4d1bb19b00601efb5a64f7d00
vincennlin <vincentagwa@gmail.com>
2026-07-02 16:40:12 +0800

    Trees - Same Binary Tree - my & ans

--- trees/easy/same-binary-tree/Solution.cpp
+++ trees/easy/same-binary-tree/Solution.cpp
@@ -7,6 +7,14 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true;
+
+        if (p != nullptr && q != nullptr) {
+            return p->val == q->val
+            && isSameTree(p->left, q->left)
+            && isSameTree(p->right, q->right);
+        }
+
         return false;
     }
 };
