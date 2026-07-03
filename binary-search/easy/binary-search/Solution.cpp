e8832c563650c7ec4d059e6e74bc9a6d9c383369
vincennlin <vincentagwa@gmail.com>
2026-07-03 10:33:26 +0800

    Binary Search - Binary Search - my & ans

--- binary-search/easy/binary-search/Solution.cpp
+++ binary-search/easy/binary-search/Solution.cpp
@@ -7,6 +7,21 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        return 0;
+        int n = nums.size();
+        int left = 0, right = n - 1;\
+        int mid;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid -1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
     }
 };
