48d5ee6a71690e0e03488f59a71fafa34225c34f
vincennlin <vincentagwa@gmail.com>
2026-07-03 10:25:10 +0800

    Stack - Valid Parentheses - ans

--- stack/easy/valid-parentheses/Solution.cpp
+++ stack/easy/valid-parentheses/Solution.cpp
@@ -2,11 +2,34 @@
 // Created by vincenn lin on 2026/7/3.
 //
 #include <string>
+#include <stack>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
     bool isValid(string s) {
-        return false;
+        if (s.size() % 2 != 0) return false;
+
+        stack<char> st;
+        unordered_map<char, char> closeToOpen = {
+            {')', '('},
+            {'}', '{'},
+            {']', '['}
+        };
+
+        for (char c : s) {
+            if (closeToOpen.count(c)) {
+                if (!st.empty() && st.top() == closeToOpen[c]) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                st.push(c);
+            }
+        }
+
+        return st.empty();
     }
 };
