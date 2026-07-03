45c1baf14e22322a44797689f5de36cad46cfaa9
vincennlin <vincentagwa@gmail.com>
2026-07-03 09:45:41 +0800

    Two Pointers - Valid Palindrome - ans

--- two-pointers/easy/valid-palindrome/Solution.cpp
+++ two-pointers/easy/valid-palindrome/Solution.cpp
@@ -8,6 +8,24 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(string s) {
-        return false;
+        if (s.size() <= 1) return true;
+
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+            if (left < right && tolower(s[left] ) != tolower(s[right])) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
     }
 };
