69f0e4affc34555060ad0cd5003f49080bdbfddf
vincennlin <vincentagwa@gmail.com>
2026-07-03 09:48:55 +0800

    Sliding Window - Best Time to Buy and Sell Stock

---
+++ sliding-window/easy/best-time-to-buy-and-sell-stock/Solution.cpp
@@ -0,0 +1,12 @@
+//
+// Created by vincenn lin on 2026/7/3.
+//
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        return 0;
+    }
+};
---
+++ sliding-window/easy/best-time-to-buy-and-sell-stock/main.cpp
@@ -0,0 +1,58 @@
+//
+// Created by vincenn lin on 2026/7/3.
+//
+/*
+*Best Time to Buy and Sell Stock
+Easy
+Topics
+Company Tags
+
+Hints
+You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+
+You may choose a single day to buy one NeetCoin and choose a different day in the future to sell it.
+
+Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
+
+Example 1:
+
+Input: prices = [10,1,5,6,7,1]
+
+Output: 6
+Explanation: Buy prices[1] and sell prices[4], profit = 7 - 1 = 6.
+
+Example 2:
+
+Input: prices = [10,8,7,5,2]
+
+Output: 0
+Explanation: No profitable transactions can be made, thus the max profit is 0.
+
+Constraints:
+
+1 <= prices.length <= 100
+0 <= prices[i] <= 100
+*/
+#include <iostream>
+#include <vector>
+#include "Solution.cpp"
+
+int main() {
+    Solution solution;
+
+    std::cout << "\n[Example 1]" << std::endl;
+    std::vector<int> prices_ex1 = {10, 1, 5, 6, 7, 1};
+    int output_ex1 = solution.maxProfit(prices_ex1);
+    std::cout << "Input: prices = [10, 1, 5, 6, 7, 1]" << std::endl;
+    std::cout << "Expected Output: 6" << std::endl;
+    std::cout << "Your Output: " << output_ex1 << std::endl;
+
+    std::cout << "\n[Example 2]" << std::endl;
+    std::vector<int> prices_ex2 = {10, 8, 7, 5, 2};
+    int output_ex2 = solution.maxProfit(prices_ex2);
+    std::cout << "Input: prices = [10, 8, 7, 5, 2]" << std::endl;
+    std::cout << "Expected Output: 0" << std::endl;
+    std::cout << "Your Output: " << output_ex2 << std::endl;
+
+    return 0;
+}
