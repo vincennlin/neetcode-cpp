37ba36df048d973702a5d1ec8734c7d7db008213
vincennlin <vincentagwa@gmail.com>
2026-07-06 10:20:33 +0800

    Arrays & Hashing - Valid Sudoku - ans - Bit

--- arrays-&-hashing/medium/valid-sudoku/Solution.cpp
+++ arrays-&-hashing/medium/valid-sudoku/Solution.cpp
@@ -11,24 +11,26 @@ using namespace std;
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<int, unordered_set<char>> rows, cols;
-        map<pair<int, int>, unordered_set<char>> squares;
+        int rows[9] = {};
+        int cols[9] = {};
+        int squares[9] = {};
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] == '.') continue;
 
-                pair<int, int> squareKey = {i / 3, j / 3};
+                int val = board[i][j] - '1';
+                int mask = 1 << val;
 
-                if (rows[i].count(board[i][j])
-                    || cols[j].count(board[i][j])
-                    || squares[squareKey].count(board[i][j])) {
+                if (rows[i] & mask
+                    || cols[j] & mask
+                    || squares[(i / 3) * 3 + (j / 3)] & mask) {
                     return false;
                 }
 
-                rows[i].insert(board[i][j]);
-                cols[j].insert(board[i][j]);
-                squares[squareKey].insert(board[i][j]);
+                rows[i] |= mask;
+                cols[j] |= mask;
+                squares[(i / 3) * 3 + (j / 3)] |= mask;
             }
         }
 
