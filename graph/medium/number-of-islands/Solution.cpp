9423485813b98c8b7f1e357a0a20add803d1131f
vincennlin <vincentagwa@gmail.com>
2026-07-06 14:46:06 +0800

    Graph - Number of Islands - ans

--- graph/medium/number-of-islands/Solution.cpp
+++ graph/medium/number-of-islands/Solution.cpp
@@ -6,31 +6,30 @@
 using namespace std;
 
 class Solution {
+    int dirs[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 public:
     int numIslands(vector<vector<char>>& grid) {
-        vector<vector<int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
         int islands = 0;
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == '1') {
-                    grid[i][j] = '0';
                     islands++;
-                    dfs(grid, dirs, i, j);
+                    dfs(grid, i, j);
                 }
             }
         }
         return islands;
     }
 
-    void dfs(vector<vector<char>>& grid, vector<vector<int>>& dirs, int i, int j) {
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
         for (auto& dir : dirs) {
-            int newI = i + dir[0];
-            int newJ = j + dir[1];
-            if (newI >= 0 && newI < grid.size() && newJ >= 0 && newJ < grid[0].size()
-                && grid[newI][newJ] == '1') {
-                grid[newI][newJ] = '0';
-                dfs(grid, dirs, newI, newJ);
-            }
+            dfs(grid, i + dir[0], j + dir[1]);
         }
     }
 };
