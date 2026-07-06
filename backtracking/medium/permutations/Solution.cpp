6a3eabd8204b1cb8e00b02355612a859802de761
vincennlin <vincentagwa@gmail.com>
2026-07-06 14:20:24 +0800

    Backtracking - Permutations - ans

--- backtracking/medium/permutations/Solution.cpp
+++ backtracking/medium/permutations/Solution.cpp
@@ -8,6 +8,21 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        return {};
+        vector<vector<int>> res;
+        backtrack(nums, res, 0);
+        return res;
+    }
+
+    void backtrack(vector<int>& nums, vector<vector<int>>& res, int i) {
+        if (i == nums.size()) {
+            res.push_back(nums);
+            return;
+        }
+
+        for (int j = i; j < nums.size(); j++) {
+            swap(nums[i], nums[j]);
+            backtrack(nums, res, i + 1);
+            swap(nums[i], nums[j]);
+        }
     }
 };
